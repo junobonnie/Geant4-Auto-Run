c13e16ea32f3ec0b4b0bd95a9ab092e0aac0fa31
replica <60418809+junobonnie@users.noreply.github.com>
2022-04-08 23:24:52 +0900

    Update plot.py

--- build/plot.py
+++ build/plot.py
@@ -1,8 +1,9 @@
+import os
 import sys
 import matplotlib.pyplot as plt
 
-energy = sys.argv[1]
-energy_bin = sys.argv[2]
+energy = int(sys.argv[1])
+energy_bin = int(sys.argv[2])
 particle = sys.argv[3]
 exec('count_list = [' + sys.argv[4] + ']')
 mode = sys.argv[5]
@@ -22,6 +23,6 @@ plt.yscale(mode)
 figure_name = particle + '_energy_spectrum_' + mode + '.png'
 plt.savefig(output_dir + '/' + figure_name)
 
-os.system('viu ' + output_dir + '/' + figure_name '>> ' +  output_dir + '/raw_result.out')
-os.system('echo "' + figure_name + '" >> ' +  output_dir + '/raw_result.out')
-os.system('echo "' + total_count + '" >> ' +  output_dir + '/raw_result.out')
+os.system('viu -w=70 ' + output_dir + '/' + figure_name + '>> ' +  output_dir + '/raw_result.out')
+os.system('echo "\033[33m[ File : ' + figure_name + ' ] [ Total : ' + str(total_count) + ' ]\033[0m\n" >> ' +  output_dir + '/raw_result.out')
+os.system('echo " " >> ' +  output_dir + '/raw_result.out')
