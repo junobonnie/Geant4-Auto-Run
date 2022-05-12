b5f36b9485e62ee418dd43cd47b6fa35ab0dd26c
replica <60418809+junobonnie@users.noreply.github.com>
2022-05-12 23:26:55 +0900

    Update cry2mac.py

--- cry2mac.py
+++ cry2mac.py
@@ -1,3 +1,5 @@
+from particle import Particle
+
 def combine_space(text):
     text = text.strip()
     while '  ' in text:
@@ -12,22 +14,24 @@ results = '''# Can be run in batch, without graphic
 /run/verbose 0
 /event/verbose 0
 /tracking/verbose 0'''
-#particles = ['neutron', 'proton', 'pion', 'kaon', 'mu', 'e', 'gamma']
-particles = ['neutron', 'proton', 'neutron', 'neutron', 'mu-', 'e-', 'gamma']
+#particles = ['neutron', 'proton', 'pion', 'kaon', '-mu', '-e', 'gamma']
+#particles = ['neutron', 'proton', 'neutron', 'neutron', 'mu-', 'e-', 'gamma']
 with open('shower.out', 'r') as f:
     lines = f.readlines()
     for line in lines[1:]:
         line = combine_space(line)
         #print(line.split(' '))
         particle_id, k_E, x, y, z, vx, vy, vz = line.split(' ')[2:]
-        particle_name = particles[int(particle_id)]
+        particle_name = Particle.from_pdgid(particle_id).name
+        if particle_name == 'n':
+            particle_name = 'neutron'
         results += '''
 /gun/particle %s
 /gun/energy %s MeV
-/gun/position %s %s -5 m
-/gun/direction %s %s %s
+/gun/position %s %s -0.5 m
+/gun/direction %s %s %f
 /run/beamOn 1
-''' %(particle_name, k_E, x, y, vx, vy, vz)
+''' %(particle_name, k_E, x, y, vx, vy, -float(vz))
 
 print(results)
 print(len(lines))
