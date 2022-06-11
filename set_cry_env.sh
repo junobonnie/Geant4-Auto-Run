8e4ac2cc4bbf876b5579e79d6233bb235ab8d86e
replica <60418809+junobonnie@users.noreply.github.com>
2022-06-11 15:51:38 +0900

    Update set_cry_env.sh

--- set_cry_env.sh
+++ set_cry_env.sh
@@ -1,13 +1,12 @@
 printf '\033[32mSetting CRY Environment\033[0m\n\n'
 
-printf "Set cry home directory location : "
+printf "Set cry test directory location : "
 read DIR
-export CRY_HOME=$DIR
-echo "OK!"
+export CRY_TEST_DIR=$DIR
+echo "\033[32mOK!\033[0m"
 
 sudo chmod 777 *.sh
 
-#ln -s $CRY_HOME/data .
-cp ./testOut.cc $CRY_HOME/test
-cd $CRY_HOME/test
+cp ./testOut.cc $CRY_TEST_DIR
+cd $CRY_TEST_DIR
 make testOut
