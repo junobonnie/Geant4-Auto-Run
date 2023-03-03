93573646cdfb736f6ef91ada455692ca8f7cebf2
replica <60418809+junobonnie@users.noreply.github.com>
2023-03-03 19:03:28 +0900

    Update plot.py

--- plot.py
+++ plot.py
@@ -11,18 +11,18 @@ output_dir = sys.argv[6]
 
 energy_list = list(range(0, energy, energy_bin))
 total_count = sum(count_list)
+if not total_count == 0:
+    plt.figure(figsize = (10,5), dpi = 720)
+    plt.text(0, max(count_list), 'Total Number of ' + particle + ': ' + str(total_count))
+    plt.bar(energy_list, count_list, width = energy_bin)
+    plt.title(particle + ' energy spectrum')
+    plt.xlabel('Energy(MeV)')
+    plt.ylabel('Count')
+    plt.yscale(mode)
 
-plt.figure(figsize = (10,5), dpi = 720)
-plt.text(0, max(count_list), 'Total Number of ' + particle + ': ' + str(total_count))
-plt.bar(energy_list, count_list, width = energy_bin)
-plt.title(particle + ' energy spectrum')
-plt.xlabel('Energy(MeV)')
-plt.ylabel('Count')
-plt.yscale(mode)
+    figure_name = particle + '_energy_spectrum_' + mode + '.png'
+    plt.savefig(output_dir + '/' + figure_name)
 
-figure_name = particle + '_energy_spectrum_' + mode + '.png'
-plt.savefig(output_dir + '/' + figure_name)
-
-os.system('viu -w=70 ' + output_dir + '/' + figure_name + '>> ' +  output_dir + '/raw_result.out')
-os.system('echo "\033[33m[ File : ' + figure_name + ' ] [ Total : ' + str(total_count) + ' ]\033[0m\n" >> ' +  output_dir + '/raw_result.out')
-os.system('echo " " >> ' +  output_dir + '/raw_result.out')
+    os.system('viu -w=70 ' + output_dir + '/' + figure_name + '>> ' +  output_dir + '/raw_result.out')
+    os.system('echo "\033[33m[ File : ' + figure_name + ' ] [ Total : ' + str(total_count) + ' ]\033[0m\n" >> ' +  output_dir + '/raw_result.out')
+    os.system('echo " " >> ' +  output_dir + '/raw_result.out')
